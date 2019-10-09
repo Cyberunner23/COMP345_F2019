32933beaa6f4fb3b1ae7c73286b891acc701face
FLAFL <narutoxela@gmail.com>
2019-10-09 17:02:39 -0400

    Started implementing map

--- src/Map/Map.cpp
+++ src/Map/Map.cpp
@@ -1,5 +1,51 @@
-//
-// Created by cyberunner23 on 10/9/19.
-//
-
 #include "Map.h"
+
+
+SGraph& Map::createSubGraph()
+{
+    return _mainGraph.create_subgraph();
+}
+
+SGraph& Map::createSubGraph(SGraph &subGraph)
+{
+    return subGraph.create_subgraph();
+}
+
+Vertex Map::addRegion(const CountryNode& region)
+{
+    Vertex v = boost::add_vertex(_mainGraph);
+
+    VertexDataPropertyMap dataMap = boost::get(vertex_data, _mainGraph);
+    VertexDisplaytxtPropertyMap displayMap = boost::get(vertex_displaytxt, _mainGraph);
+
+    displayMap[v] = region._name;
+    dataMap[v] = region;
+
+    return v;
+}
+
+Vertex Map::addRegion(const Vertex& regionVertex, SGraph& subGraph)
+{
+    return boost::add_vertex(regionVertex, subGraph);
+}
+
+Edge Map::connectRegion(const Vertex& v1, const Vertex& v2)
+{
+    return connectRegion(v1, v2, _mainGraph);
+}
+
+Edge Map::connectRegion(const Vertex& v1, const Vertex& v2, SGraph& subGraph)
+{
+    return boost::add_edge(v1, v2, subGraph).first;
+}
+
+bool Map::isConnected()
+{
+    return isConnected(_mainGraph);
+}
+
+bool Map::isConnected(SGraph &graph)
+{
+    std::vector<int> component(boost::num_vertices(graph));
+    return boost::connected_components(graph, &component[0]) == 1;
+}
--- src/Map/Map.h
+++ src/Map/Map.h
@@ -41,7 +41,96 @@ SERIALIZE_CLEANUP(std::vector<std::string>)
 // Map
 //---------------------------------------------------------
 
+// Custom property for node data
+enum vertex_data_t
+{
+    vertex_data
+};
+
+// Custom property for the display text of the node.
+// Will be used to display text in the cells in the graphviz export
+enum vertex_displayTxt_t
+{
+    vertex_displayTxt
+};
+
+// Registering the new properties
+namespace boost
+{
+    BOOST_INSTALL_PROPERTY(vertex, data);
+    BOOST_INSTALL_PROPERTY(vertex, displayTxt);
+}
+
+// The graph. Uses Boost::graph.
+// Created a graph with an adjacency list implementation.
+// It includes the custom properties I defined above.
+typedef boost::subgraph<
+        boost::adjacency_list<
+                boost::vecS,
+                boost::vecS,
+                boost::undirectedS,
+                boost::property<boost::vertex_index_t, int,
+                boost::property<vertex_data_t, CountryNode,
+                boost::property<vertex_displayTxt_t, std::string>>>,
+                boost::property<boost::edge_index_t, int>
+        >> SGraph;
+
+// Type for the vertices
+typedef typename boost::graph_traits<SGraph>::vertex_descriptor Vertex;
+// Type for the edges
+typedef typename boost::graph_traits<SGraph>::edge_descriptor Edge;
+
+// Type for the vertex iterator, used to go through the list of vertices
+typedef typename boost::graph_traits<SGraph>::vertex_iterator VertexIterator;
+
+// Type for the PropertyMap of the vertex data.
+// Used to extract the node data.
+typedef typename boost::property_map<SGraph, vertex_data_t>::type VertexDataPropertyMap;
+// Type for the PropertyMap of the DisplayTxt, used to get/set the data to be displayed in the graphVis output.
+typedef typename boost::property_map<SGraph, vertex_displayTxt_t>::type VertexDisplayTxtPropertyMap;
+
 class Map
 {
 
+    Map() = default;
+
+    SGraph* getGraph()
+    {
+        return _mainGraph;
+    }
+
+    void setGraph(SGraph* graph)
+    {
+        _mainGraph = graph;
+    }
+
+    // Creates a sub-graph from the main graph.
+    SGraph& createSubGraph();
+
+    // Creates a sub-graph from the the provided sub-graph.
+    SGraph& createSubGraph(SGraph& subGraph);
+
+    // Adds a RegionNode to the main graph.
+    Vertex addRegion(const CountryNode& region);
+
+    // Adds a RegionNode to the provided sub-graph.
+    Vertex addRegion(const Vertex& regionVertex, SGraph& subGraph);
+
+    // Creates an edge between the provided region vertices from the main graph.
+    // Used when generating maps from code.
+    Edge connectRegion(const Vertex& v1, const Vertex& v2);
+
+    // Creates an edge between the provided region vertices from a subgraph.
+    // Used when generating maps from code.
+    Edge connectRegion(const Vertex& v1, const Vertex& v2, SGraph& subGraph);
+
+    // Verifies whether the main graph is connected or not
+    bool isConnected();
+
+    // Verifies whether the provided subgraph is connected or not.
+    bool isConnected(SGraph& subGraph);
+
+private:
+
+    SGraph* _mainGraph;
 };
