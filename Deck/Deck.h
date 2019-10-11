8c8b14cdb570572fd53d0689459bf81275fd54c7
Alex-Hayeur <31771444+Alex-Hayeur@users.noreply.github.com>
2019-10-11 11:05:51 -0400

    Updating for card cost & hand
    Currently card costs are just garbage values, and can be set through the enum

--- Deck/Deck.h
+++ Deck/Deck.h
@@ -12,7 +12,7 @@
 //https://boardgamegeek.com/article/12349266#12349266 <<List of cards in game
   //Total of 42 cards
   enum Cards{
-    f1, f2,f3, f4, f5, f6, f7, f8,
+    f1 f2,f3, f4, f5, f6, f7, f8,
     c1, c2, c3, c4, c5, c6, c7, c8, c9, c10,
     a1, a2, a3, a4, a5, a6, a7, a8, a9,
     o1, o2, o3, o4, o5, o6, o7,
@@ -25,12 +25,15 @@
       o1, o2, o3, o4, o5, o6, o7,
       cr1, cr2, cr3, cr4, cr5};
 
+  std::vector<Cards> Hand;
+
       Cards draw()
       {
         Cards c;
         if(!Deck.empty())
         {
         c = Deck.back();
+        Hand.push_back(c);
         Deck.pop_back();
         }
         return c;
--- Deck/DeckDriver.cpp
+++ Deck/DeckDriver.cpp
@@ -4,22 +4,28 @@ int main() {
   std::string response;
 
 
- unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
- std::shuffle(Deck.begin(), Deck.end(), std::default_random_engine(seed));
+  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  std::shuffle(Deck.begin(), Deck.end(), std::default_random_engine(seed));
 
-  std::cout <<  "You Have Drawn: " << DeckMap[draw()] << std::endl;
+  Cards c = draw();
+  std::cout <<  "You Have Drawn: " << DeckMap[draw()] << ", Cost: "<< c <<std::endl;
   std::cout << "Draw again? Y/N" << std::endl;
   std::cin >> response;
 
   while(response != "n")
   {
-
-    std::cout <<  "You Have Drawn: " << DeckMap[draw()] << std::endl;
+    c = draw();
+    std::cout <<  "You Have Drawn: " << DeckMap[c] << ", Cost: "<< c << std::endl;
     std::cout << "Draw again? Y/N" << std::endl;
     std::cin >> response;
 
   }
 
+  std::cout << "Your hand is: " << std::endl;
+  for(int i = 0; i < Hand.size(); i++)
+  {
+    std::cout << DeckMap[Hand[i]] << ", Cost: "<< Hand[i] <<std::endl;
+  }
 
   return 0;
 };
