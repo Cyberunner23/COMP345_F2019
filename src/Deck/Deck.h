a59b038ea9d32a63fff604cca60e686ad8a7a913
Alex Frappier Lachapelle <narutoxela@gmail.com>
2019-10-11 16:05:54 -0400

    Fixed build

--- src/Deck/Deck.h
+++ src/Deck/Deck.h
@@ -12,7 +12,7 @@
 //https://boardgamegeek.com/article/12349266#12349266 <<List of cards in game
   //Total of 42 cards
   enum Cards{
-    f1 f2,f3, f4, f5, f6, f7, f8,
+    f1, f2,f3, f4, f5, f6, f7, f8,
     c1, c2, c3, c4, c5, c6, c7, c8, c9, c10,
     a1, a2, a3, a4, a5, a6, a7, a8, a9,
     o1, o2, o3, o4, o5, o6, o7,
