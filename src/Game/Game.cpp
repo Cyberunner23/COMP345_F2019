90cdebd9b9f40b74c9b71e46d35558b8702f63bb
wil59 <wil59@DESKTOP-ANHNNE5>
2019-11-03 19:27:48 -0500

    Finished part5 driver logic

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -61,3 +61,13 @@ std::vector<Player>* Game::createPlayers()
 
 	return players;
 }
+
+void Game::displayFaceUpCards(GameState state)
+{
+	int position = 1;
+	std::cout << "------- Displaying Cards -------" << std::endl;
+	for (auto& card : *state.ShownCards) {
+		std::cout << "Position: " << position << ", Action: " << state.GameDeck->DeckMap->at(card) << std::endl;
+		position++;
+	}
+}
--- src/Game/Game.h
+++ src/Game/Game.h
@@ -3,10 +3,12 @@
 #include <iostream>
 #include <vector>
 #include "Player/Player.h"
+#include <Game\GameState.hpp>
 
 class Game {
 
 	public:
 		static std::string selectMap();
 		static std::vector<Player>* createPlayers();
+		static void displayFaceUpCards(GameState state);
 };
