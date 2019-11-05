d9e334844f8f41910967f7ca627e8552a24573a6
Alex Frappier Lachapelle <narutoxela@gmail.com>
2019-11-04 22:56:41 -0500

    finalized part 6

--- src/Game/GameScoreDriver.cpp
+++ src/Game/GameScoreDriver.cpp
@@ -8,96 +8,121 @@
 
 int main(int argc, char** argv)
 {
-	GameState state;
-	MapLoader loader;
-	std::string mapPath;
+    GameState state;
+    MapLoader loader;
+    std::string mapPath;
 
-	mapPath = Game::selectMap();
+    mapPath = Game::selectMap();
 
-	Map* map = loader.loadMap(mapPath);
-	if (map == nullptr)
-	{
-		std::cout << "Failed to open map!" << std::endl;
-	}
+    Map *map = loader.loadMap(mapPath);
+    if (map == nullptr)
+    {
+        std::cout << "Failed to open map!" << std::endl;
+    }
 
-	std::cout << "Generating the game..." << std::endl;
-	state.GameMap = map;
+    std::cout << "Generating the game..." << std::endl;
+    state.GameMap = map;
 
-	state.GameDeck = new Deck(); //Instantiating the Deck
-	state.GameDeck->shuffleDeck();//Shuffling the deck
+    state.GameDeck = new Deck(); //Instantiating the Deck
+    state.GameDeck->shuffleDeck();//Shuffling the deck
 
-	//Initialize Players
-	state.Players = new std::vector<Player>();
+    //Initialize Players
+    state.Players = new std::vector<Player>();
 
-	std::string* nameP1 = new std::string("Player 1");
-	std::string* nameP2 = new std::string("Player 2");
-	std::string* nameP3 = new std::string("Player 3");
+    std::string *nameP1 = new std::string("Player 1");
+    std::string *nameP2 = new std::string("Player 2");
+    std::string *nameP3 = new std::string("Player 3");
 
-	int* ageP1 = new int(23);
-	int* ageP2 = new int(21);
-	int* ageP3 = new int(22);
+    int *ageP1 = new int(23);
+    int *ageP2 = new int(21);
+    int *ageP3 = new int(22);
 
-	int* coinP1 = new int(2);
-	int* coinP2 = new int(6);
-	int* coinP3 = new int(3);
+    int *coinP1 = new int(2);
+    int *coinP2 = new int(6);
+    int *coinP3 = new int(3);
 
-	Player p1;
-	p1.setName(nameP1);
-	p1.setage(ageP1);
-	p1.setCityColor(Cities::BLUE);
-	p1.setArmyColor(Armies::BLUE);
-	p1.setCoins(coinP1);
-	for (int i = 0; i < 10; i++) { //Gives 6 cards to Player 1
-		Cards c = state.GameDeck->draw();
-		p1.getHand()->HandList->push_back(c);
-	}
+    Player p1;
+    p1.setName(nameP1);
+    p1.setage(ageP1);
+    p1.setCityColor(Cities::BLUE);
+    p1.setArmyColor(Armies::BLUE);
+    p1.setCoins(coinP1);
+    p1.getHand()->HandList->push_back(c6);
 
-	Player p2;
-	p2.setName(nameP2);
-	p2.setage(ageP2);
-	p2.setCityColor(Cities::GREEN);
-	p2.setArmyColor(Armies::GREEN);
-	p2.setCoins(coinP2);
-	for (int i = 0; i < 13; i++) { //Gives 9 cards to Player 2
-		Cards c = state.GameDeck->draw();
-		p2.getHand()->HandList->push_back(c);
-	}
+    Player p2;
+    p2.setName(nameP2);
+    p2.setage(ageP2);
+    p2.setCityColor(Cities::GREEN);
+    p2.setArmyColor(Armies::GREEN);
+    p2.setCoins(coinP2);
 
-	Player p3;
-	p3.setName(nameP3);
-	p3.setage(ageP3);
-	p3.setCityColor(Cities::RED);
-	p3.setArmyColor(Armies::RED);
-	p3.setCoins(coinP3);
-	for (int i = 0; i < 11; i++) { //Give 4 cards to Player 3
-		Cards c = state.GameDeck->draw();
-		p3.getHand()->HandList->push_back(c);
-	}
+    p2.getHand()->HandList->push_back(cr3);
+    p2.getHand()->HandList->push_back(a1);
+    p2.getHand()->HandList->push_back(o2);
+    p2.getHand()->HandList->push_back(c3);
+    p2.getHand()->HandList->push_back(a6);
+    p2.getHand()->HandList->push_back(a8);
+    p2.getHand()->HandList->push_back(cr5);
+    p2.getHand()->HandList->push_back(a9);
+    p2.getHand()->HandList->push_back(o1);
+    p2.getHand()->HandList->push_back(cr2);
+    p2.getHand()->HandList->push_back(f6);
+    p2.getHand()->HandList->push_back(f7);
+    p2.getHand()->HandList->push_back(o4);
 
-	state.Players->push_back(p1);
-	state.Players->push_back(p2);
-	state.Players->push_back(p3);
+    Player p3;
+    p3.setName(nameP3);
+    p3.setage(ageP3);
+    p3.setCityColor(Cities::RED);
+    p3.setArmyColor(Armies::RED);
+    p3.setCoins(coinP3);
 
-	//Retrieving 4 countries
-	CountryNode* country1 = state.GameMap->findCountryByID(3);
-	CountryNode* country2 = state.GameMap->findCountryByID(6);
-	CountryNode* country3 = state.GameMap->findCountryByID(5);
-	CountryNode* country4 = state.GameMap->findCountryByID(1);
+    p3.getHand()->HandList->push_back(c1);
+    p3.getHand()->HandList->push_back(a3);
+    p3.getHand()->HandList->push_back(o3);
+    p3.getHand()->HandList->push_back(a4);
+    p3.getHand()->HandList->push_back(c8);
+    p3.getHand()->HandList->push_back(f5);
+    p3.getHand()->HandList->push_back(f1);
+    p3.getHand()->HandList->push_back(f8);
+    p3.getHand()->HandList->push_back(a2);
+    p3.getHand()->HandList->push_back(c9);
+    p3.getHand()->HandList->push_back(a7);
 
-	//Adding armies to countries
-	country1->ArmiesInCountry->push_back(p1.getArmyColor());
-	country1->ArmiesInCountry->push_back(p1.getArmyColor());
-	country2->ArmiesInCountry->push_back(p3.getArmyColor());
-	country2->ArmiesInCountry->push_back(p2.getArmyColor());
-	country2->ArmiesInCountry->push_back(p2.getArmyColor());
-	country3->ArmiesInCountry->push_back(p3.getArmyColor());
-	country3->ArmiesInCountry->push_back(p3.getArmyColor());
-	country4->ArmiesInCountry->push_back(p1.getArmyColor());
-	country4->ArmiesInCountry->push_back(p3.getArmyColor());
-	country4->ArmiesInCountry->push_back(p2.getArmyColor());
 
-	ScoreCalculator sc(&state);
-	sc.CalculateScores();
+    state.Players->push_back(p1);
+    state.Players->push_back(p2);
+    state.Players->push_back(p3);
 
-	return 0;
+    //Retrieving 4 countries
+    CountryNode *country1 = state.GameMap->findCountryByID(3);
+    CountryNode *country2 = state.GameMap->findCountryByID(6);
+    CountryNode *country3 = state.GameMap->findCountryByID(5);
+    CountryNode *country4 = state.GameMap->findCountryByID(1);
+
+    //Adding armies to countries
+    country1->ArmiesInCountry->push_back(p1.getArmyColor());
+    country1->ArmiesInCountry->push_back(p1.getArmyColor());
+
+    country2->ArmiesInCountry->push_back(p3.getArmyColor());
+    country2->ArmiesInCountry->push_back(p2.getArmyColor());
+    country2->ArmiesInCountry->push_back(p2.getArmyColor());
+
+    country3->ArmiesInCountry->push_back(p3.getArmyColor());
+    country3->ArmiesInCountry->push_back(p3.getArmyColor());
+
+    country4->ArmiesInCountry->push_back(p1.getArmyColor());
+    country4->ArmiesInCountry->push_back(p3.getArmyColor());
+    country4->ArmiesInCountry->push_back(p2.getArmyColor());
+
+    ScoreCalculator sc(&state);
+    sc.CalculateScores();
+
+    std::cout << "Score for Player 1 should have been 2" << std::endl;
+    std::cout << "Score for Player 2 should have been 9" << std::endl;
+    std::cout << "Score for Player 3 should have been 17" << std::endl;
+
+    std::cout << "Player 3 should have won" << std::endl;
+
+    return 0;
 }
--- src/ScoreCalculator/ScoreCalculator.cpp
+++ src/ScoreCalculator/ScoreCalculator.cpp
@@ -9,7 +9,7 @@ ScoreCalculator::ScoreCalculator(GameState* s)
 void ScoreCalculator::CalculateScores()
 {
 
-    int currentScore;
+    //int currentScore;
     std::vector<Cards> currentHand;
     std::vector<Armies> regionScores;
 
@@ -19,110 +19,129 @@ void ScoreCalculator::CalculateScores()
     int Ores = 0;
     int Crystals = 0;
 
-  //Calculating Scores based on Goods
-  currentScore = 0;
+    //Calculating Scores based on Goods
+    //currentScore = 0;
 
-  for(int i = 0; i < State->Players->size(); i++)
-  {
-      currentHand = *State->Players->at(i).getHand()->HandList;
-      currentScore = *State->Players->at(i).getScore();
-
-      for(int j = 0; j < currentHand.size(); j++)
-      {
-          std::string currentGood = State->GameDeck->GoodsMap->at(currentHand.at(j));
-
-          if (currentGood == "Forest")
-          {
-              Forests++;
-          }
-          else if (currentGood == "Carrots")
-          {
-            Carrots++;
-          }
-          else if (currentGood == "Anvils")
-          {
-              Anvils++;
-          }
-          else if (currentGood == "Ore")
-          {
-              Ores++;
-          }
-          else if (currentGood == "Crystals")
-          {
-              Crystals++;
-          }
-      }
-
-      //Scoring of Forests
-      if(Forests >= 6 ) {currentScore += 5;}
-      else if(Forests == 5){currentScore += 3;}
-      else if(Forests == 4 || Forests == 3){currentScore += 2;}
-      else if(Forests == 2){currentScore += 1;}
-
-      //Scoring of Carrots
-      if(Carrots >= 8 ) {currentScore += 5;}
-      else if(Carrots == 7){currentScore += 3;}
-      else if(Carrots == 6 || Carrots == 5){currentScore += 2;}
-      else if(Carrots == 4 || Carrots == 3){currentScore += 1;}
-
-      //Scoring of Anvil
-      if(Anvils >= 7 ) {currentScore += 5;}
-      else if(Anvils == 6){currentScore += 3;}
-      else if(Anvils == 5 || Anvils == 4){currentScore += 2;}
-      else if(Anvils == 3 || Anvils == 2){currentScore += 1;}
-      //Scoring of Ores
-      if(Ores >= 5 ) {currentScore += 5;}
-      else if(Ores == 4){currentScore += 3;}
-      else if(Ores == 3){currentScore += 2;}
-      else if(Ores == 2){currentScore += 1;}
-      //Scoring of Crystals
-      if(Crystals >= 4 ) {currentScore += 5;}
-      else if(Crystals == 3){currentScore += 3;}
-      else if(Crystals == 2){currentScore += 2;}
-      else if(Crystals == 1){currentScore += 1;}
-
-      State->Players->at(i).setScore(&currentScore);
-
-  }
-  //Calculate Scores based on countries owned
-regionScores = State->GameMap->getCountryOwners();
-for(int i = 0; i < State->Players->size(); i++)
-{
-  for(int j = 0; j < regionScores.size(); j++)
-  {
-    if(State->GameDeck->ArmiesMap->at(State->Players->at(i).getArmyColor()) == State->GameDeck->ArmiesMap->at(regionScores.at(j)))
+    for (int i = 0; i < State->Players->size(); i++)
     {
-        (*State->Players->at(i).getScore())++;
-    }
-    //State -> Players -> at(i).setScore(&currentScore);
-  }
-}
+        currentHand = *State->Players->at(i).getHand()->HandList;
+        //currentScore = 0;
 
-//Calculating scores based on continents owned
-regionScores = State->GameMap->getContinentOwners();
-for(int i = 0; i < State->Players->size(); i++)
-{
-  //currentScore = ;
-  for(int j = 0; j < regionScores.size(); j++)
-  {
-    if(State->GameDeck->ArmiesMap->at(State->Players->at(i).getArmyColor()) == State->GameDeck->ArmiesMap->at(regionScores.at(j)))
+        for (int j = 0; j < currentHand.size(); j++)
+        {
+            std::string currentGood = State->GameDeck->GoodsMap->at(currentHand.at(j));
+
+            if (currentGood == "Forest")
+            {
+                Forests++;
+            }
+            else if (currentGood == "Carrots")
+            {
+                Carrots++;
+            }
+            else if (currentGood == "Anvils")
+            {
+                Anvils++;
+            }
+            else if (currentGood == "Ore")
+            {
+                Ores++;
+            }
+            else if (currentGood == "Crystals")
+            {
+                Crystals++;
+            }
+        }
+
+        //Scoring of Forests
+        if (Forests >= 6)
+        { (*State->Players->at(i).getScore()) += 5; }
+        else if (Forests == 5)
+        { (*State->Players->at(i).getScore()) += 3; }
+        else if (Forests == 4 || Forests == 3)
+        { (*State->Players->at(i).getScore()) += 2; }
+        else if (Forests == 2)
+        { (*State->Players->at(i).getScore()) += 1; }
+
+        //Scoring of Carrots
+        if (Carrots >= 8)
+        { (*State->Players->at(i).getScore()) += 5; }
+        else if (Carrots == 7)
+        { (*State->Players->at(i).getScore()) += 3; }
+        else if (Carrots == 6 || Carrots == 5)
+        { (*State->Players->at(i).getScore()) += 2; }
+        else if (Carrots == 4 || Carrots == 3)
+        { (*State->Players->at(i).getScore()) += 1; }
+
+        //Scoring of Anvil
+        if (Anvils >= 7)
+        { (*State->Players->at(i).getScore()) += 5; }
+        else if (Anvils == 6)
+        { (*State->Players->at(i).getScore()) += 3; }
+        else if (Anvils == 5 || Anvils == 4)
+        { (*State->Players->at(i).getScore()) += 2; }
+        else if (Anvils == 3 || Anvils == 2)
+        { (*State->Players->at(i).getScore()) += 1; }
+        //Scoring of Ores
+        if (Ores >= 5)
+        { (*State->Players->at(i).getScore()) += 5; }
+        else if (Ores == 4)
+        { (*State->Players->at(i).getScore()) += 3; }
+        else if (Ores == 3)
+        { (*State->Players->at(i).getScore()) += 2; }
+        else if (Ores == 2)
+        { (*State->Players->at(i).getScore()) += 1; }
+        //Scoring of Crystals
+        if (Crystals >= 4)
+        { (*State->Players->at(i).getScore()) += 5; }
+        else if (Crystals == 3)
+        { (*State->Players->at(i).getScore()) += 3; }
+        else if (Crystals == 2)
+        { (*State->Players->at(i).getScore()) += 2; }
+        else if (Crystals == 1)
+        { (*State->Players->at(i).getScore()) += 1; }
+
+        //*State->Players->at(i).getScore() = currentScore;
+    }
+
+    //Calculate Scores based on countries owned
+    regionScores = State->GameMap->getCountryOwners();
+    for (int i = 0; i < State->Players->size(); i++)
     {
-        (*State->Players->at(i).getScore())++;
+        for (int j = 0; j < regionScores.size(); j++)
+        {
+            if (State->GameDeck->ArmiesMap->at(State->Players->at(i).getArmyColor()) ==
+                State->GameDeck->ArmiesMap->at(regionScores.at(j)))
+            {
+                (*State->Players->at(i).getScore())++;
+            }
+        }
     }
-    //State -> Players -> at(i).setScore(&currentScore);
-  }
-}
 
-//Calculating highest score
-currentScore = 0;
-for(int i = 0; i < State->Players->size(); i++)
-{
-  if(currentScore < *State->Players->at(i).getScore())
-  {
-    currentScore = *State->Players -> at(i).getScore();
-  }
-  std::cout << "Player " << i+1 << " your score is " << *State->Players->at(i).getScore() << std::endl;
-}
-std::cout << "The winning score is: " << currentScore << std::endl;
+    //Calculating scores based on continents owned
+    regionScores = State->GameMap->getContinentOwners();
+    for (int i = 0; i < State->Players->size(); i++)
+    {
+        for (int j = 0; j < regionScores.size(); j++)
+        {
+            if (State->GameDeck->ArmiesMap->at(State->Players->at(i).getArmyColor()) ==
+                State->GameDeck->ArmiesMap->at(regionScores.at(j)))
+            {
+                (*State->Players->at(i).getScore())++;
+            }
+        }
+    }
+
+    //Calculating highest score
+    int currentScore = 0;
+    for (int i = 0; i < State->Players->size(); i++)
+    {
+        if (currentScore < *State->Players->at(i).getScore())
+        {
+            currentScore = *State->Players->at(i).getScore();
+        }
+        std::cout << "Player " << i + 1 << " your score is " << *State->Players->at(i).getScore() << std::endl;
+    }
+    std::cout << "The winning score is: " << currentScore << std::endl;
 
 }
